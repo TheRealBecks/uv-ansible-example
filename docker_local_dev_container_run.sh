d239e099e31c04b47dcf5bed6adb8203396c6d42
Martin Beckert <beckert@strato.de>
2024-10-25 10:28:54 +0200

    Format docker_local_dev_container_run.sh

--- docker_local_dev_container_run.sh
+++ docker_local_dev_container_run.sh
@@ -12,28 +12,27 @@ source .env
 set +a
 
 # Start Podman/Docker in interactive mode...
-if [ -z ${COMPANY_CERTIFICATE_FOLDER+COMPANY_CERTIFICATE_FOLDER} ]
-then
+if [ -z ${COMPANY_CERTIFICATE_FOLDER+COMPANY_CERTIFICATE_FOLDER} ]; then
     # ...without COMPANY_CERTIFICATE_FOLDER environment variable
     docker run \
-    -it \
-    -w "/uv-ansible-example/uv-ansible-example" \
-    --env-file ".env" \
-    -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
-    --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
-    --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
-    uv-ansible-example \
-    bash
+        -it \
+        -w "/uv-ansible-example/uv-ansible-example" \
+        --env-file ".env" \
+        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+        --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
+        --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
+        uv-ansible-example \
+        bash
 else
     # ...with COMPANY_CERTIFICATE_FOLDER environment variable
     docker run \
-    -it \
-    -w "/uv-ansible-example/uv-ansible-example" \
-    --env-file ".env" \
-    -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
-    --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
-    --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
-    --mount type=bind,source="${COMPANY_CERTIFICATE_FOLDER}",destination="/usr/share/ca-certificates/company",readonly \
-    uv-ansible-example \
-    bash
+        -it \
+        -w "/uv-ansible-example/uv-ansible-example" \
+        --env-file ".env" \
+        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+        --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
+        --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
+        --mount type=bind,source="${COMPANY_CERTIFICATE_FOLDER}",destination="/usr/share/ca-certificates/company",readonly \
+        uv-ansible-example \
+        bash
 fi
