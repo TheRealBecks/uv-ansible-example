c8a18cdedf5372bbe498ace14a72897674e74150
Martin Beckert <beckert@strato.de>
2024-10-25 10:50:05 +0200

    Add $SOURCE_DIR in docker scripts

--- docker_local_dev_container_build.sh
+++ docker_local_dev_container_build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+#Define source directory of the Scrtipt
+SOURCE_DIR=$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")
+
 # Build the local Podman/Docker container
-docker build --tag uv-ansible-example .
+docker build --tag uv-ansible-example "$SOURCE_DIR"
--- docker_local_dev_container_run.sh
+++ docker_local_dev_container_run.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+#Define source directory of the Scrtipt
+SOURCE_DIR=$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")
+
 # Check if user has set '.env' file
-if [ ! -f .env ]; then
-    echo "Error: Specify your '.env' file first!"
+if [ ! -f "${SOURCE_DIR}/.env" ]; then
+    echo "Error: Specify your '.env' file in \"${SOURCE_DIR}\" first!"
     exit 1
 fi
 
 # Read all variables as 'export'
 set -a
-source .env
+source "${SOURCE_DIR}/.env"
 set +a
 
 # Start Podman/Docker in interactive mode...
@@ -17,10 +20,10 @@ if [ -z ${COMPANY_CERTIFICATE_FOLDER+COMPANY_CERTIFICATE_FOLDER} ]; then
     docker run \
         -it \
         -w "/uv-ansible-example/uv-ansible-example" \
-        --env-file ".env" \
-        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+        --env-file "${SOURCE_DIR}/.env" \
+        -e SSH_AUTH_SOCK="${SSH_AUTH_SOCK}" \
         --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
-        --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
+        --mount type=bind,source="${SOURCE_DIR}/uv-ansible-example",destination="/uv-ansible-example/uv-ansible-example" \
         uv-ansible-example \
         bash
 else
@@ -28,10 +31,10 @@ else
     docker run \
         -it \
         -w "/uv-ansible-example/uv-ansible-example" \
-        --env-file ".env" \
-        -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+        --env-file "${SOURCE_DIR}/.env" \
+        -e SSH_AUTH_SOCK="${SSH_AUTH_SOCK}" \
         --mount type=bind,source="${SSH_AUTH_SOCK}",destination="${SSH_AUTH_SOCK}",readonly \
-        --mount type=bind,source="$(realpath uv-ansible-example)",destination="/uv-ansible-example/uv-ansible-example" \
+        --mount type=bind,source="${SOURCE_DIR}/uv-ansible-example",destination="/uv-ansible-example/uv-ansible-example" \
         --mount type=bind,source="${COMPANY_CERTIFICATE_FOLDER}",destination="/usr/share/ca-certificates/company",readonly \
         uv-ansible-example \
         bash
